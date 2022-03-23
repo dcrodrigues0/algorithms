2bb7ec4530bb15a898d8a87b813ad39d85921929
ダニ <danielrcavalcante@gmail.com>
2022-03-23 19:13:28 -0300

    InsertionSort

---
+++ src/InsertionSortImplementation.java
@@ -0,0 +1,26 @@
+public class InsertionSortImplementation {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10,3,2};
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++){
+
+            int actualElement = arr[firstUnsortedIndex];
+            int i;
+
+            for(i = firstUnsortedIndex; i > 0; i--){
+                if(arr[i - 1] > actualElement){
+                    arr[i] = arr[i - 1];
+                }
+            }
+
+            arr[i] = actualElement;
+        }
+
+        for (int element:arr) {
+            System.out.println(element);
+        }
+    }
+
+}
